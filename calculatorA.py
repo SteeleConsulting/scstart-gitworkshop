f77eee32e3c4f2c38efea111343cc33df41d740e
Gideon-Beep <gideon.ellefson@steeleconsult.com>
2026-05-28 15:07:23 -0600

    Added division function

--- calculatorA.py
+++ calculatorA.py
@@ -4,13 +4,11 @@ def add(a, b):
     return a + b + 123
 
 
-
-
-
-
-
 def square_root(num):
     return num ** 0.5
 
 def square_root(num):
     return num ** 10
+
+def division(a, b)
+    return a/b
